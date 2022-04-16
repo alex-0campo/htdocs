42281b0b89ecc4411d419b64a138755eb2090b7c
alex-0campo <alexo.seattle@gmail.com>
2022-04-16 13:59:30 -0700

    renamed helper folder to modules

--- templates/global.php
+++ templates/global.php
@@ -1,5 +1,5 @@
 <?php
-    define('D_IMAGES', ($_SERVER['DOCUMENT_ROOT'] . 'img/'));
+    define('D_IMAGES', ($_SERVER['DOCUMENT_ROOT'] . 'images/'));
     define('D_MODULES', ($_SERVER['DOCUMENT_ROOT'] . 'modules/'));
     define('D_TEMPLATES', ($_SERVER['DOCUMENT_ROOT'] . 'templates/'));    
 ?>
