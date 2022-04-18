d2561d1c288797e01de9c1b67a1f3e8311f99bd6
alex-0campo <alexo.seattle@gmail.com>
2022-04-18 15:22:38 -0700

    explore anonymous functions

--- modules/script.js
+++ modules/script.js
@@ -1,39 +1,53 @@
-// console.log("Hello world!");
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     document
-//         .getElementById('separator')
-//         .addEventListener('input', separatorValue);
-//     document
-//         .getElementById('numwords')
-//         .addEventListener('input', numWords);
-// });
-
-let count = 2;
-// console.log(typeof count);
-
-let multiplier = 10;
-// console.log(typeof multiplier);
-// console.log(count);
-
-let doStuff = (count, multiplier) => { 
-    return count * multiplier
-    // console.log(count * multiplier);
-}
-
-// console.log(doStuff(count, multiplier));
-console.log(doStuff(5, 3));
-
-// let separator = document.getElementById('separator');
-// let numwords = document.getElementById('numwords');
-// numwords.addEventListener('input', numWords);
+document.addEventListener('DOMContentLoaded', () => {
+    document
+        .getElementById('separator')
+        .addEventListener('input', separatorValue);
+    document
+        .getElementById('numwords')
+        .addEventListener('input', numWordsValue);
+});
 
 function separatorValue(event) {
     let separator = event.target;
     console.log('Separator: ' + separator.value)
 };
 
-function numWords(event) {
+function numWordsValue(event) {
     let selected = event.target;
     console.log('WordCount: ' + selected.value)
 };
+
+
+
+
+// let doStuff = (count, multiplier) => { 
+//     return count * multiplier
+//     // console.log(count * multiplier);
+// }
+
+// document.addEventListener('DOMContentLoaded', (
+        
+//         () => {
+        
+//             let count = 2;
+//             // console.log(typeof count);
+            
+//             let multiplier = 10;
+//             // console.log(typeof multiplier);
+//             // console.log(count);  
+            
+            
+//             // console.log(doStuff(count, multiplier));
+//             console.log(doStuff(count, multiplier));
+//         }
+//     )()
+// );
+
+
+
+
+// let separator = document.getElementById('separator');
+// let numwords = document.getElementById('numwords');
+// numwords.addEventListener('input', numWords);
+
